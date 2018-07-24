a25a0f42beba24859dc2350a8fc9655fabdc9a99
katieroseschulz <katiekaterose13@gmail.com>
2018-07-24 19:43:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,14 @@
 def counter
   number = 0
   while number < 11
-  if counter % 3 == 0 && counter % 5 == 0 
+  if int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
-  elsif counter % 3 == 0 
+  elsif int % 3 == 0 
     "Fizz"
-  elsif counter % 5 == 0 
+  elsif int % 5 == 0 
     "Buzz"
   puts number
   number += 1 
-end
-end
 end
 
 counter
