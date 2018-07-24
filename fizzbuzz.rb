b6a1c37dd4b0123fecc002eb4d6a4c060405db01
katieroseschulz <katiekaterose13@gmail.com>
2018-07-24 19:36:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -20,15 +20,4 @@ end
 
 counter
 
-def fizzbuzz(counter)
-  if counter % 3 == 0 && counter % 5 == 0 
-    "FizzBuzz"
-  elsif counter % 3 == 0 
-    "Fizz"
-  elsif counter % 5 == 0 
-    "Buzz"
-  end
-end
-
-fizzbuzz(counter)
 
