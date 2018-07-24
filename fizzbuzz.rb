824ee6709203709910def5c960b21c4d73c7088f
katieroseschulz <katiekaterose13@gmail.com>
2018-07-24 19:40:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@ def counter
   puts number
   number += 1 
 end
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
+def fizzbuzz(counter)
+  if counter % 3 == 0 && counter % 5 == 0 
     "FizzBuzz"
-  elsif int % 3 == 0 
+  elsif counter % 3 == 0 
     "Fizz"
-  elsif int % 5 == 0 
+  elsif counter % 5 == 0 
     "Buzz"
   end
 end
